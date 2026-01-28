737eb91d88a62ba5d1816676090d0fb73e2d963e
SoloGudLyf <solomonteiko0@gmail.com>
2026-01-28 13:57:53 +0000

    feat: Setup passport, prisma and express

---
+++ app.js
@@ -0,0 +1,110 @@
+/////// app.js
+
+import "dotenv/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import { pool } from "./db/pool.js";
+import pgSession from "connect-pg-simple";
+import expressSession from "express-session";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg"; // For other db adapters, see Prisma docs
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+
+const pool = new Pool({
+  // add your configuration
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const PostgreSqlStore = pgSession(session);
+
+// DATABASE_URL defined in env file included in prisma.config.js; see Prisma docs
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(indexRouter);
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("app listening on port " + PORT + "!");
+});
